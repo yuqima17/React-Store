a06b6b063b1f513027f9ad1805478363b9afe7e0
yuqima17 <Yuqi.Ma@nscorp.com>
2020-02-15 16:17:04 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from 'Router';
+import 'css/style.scss';
+import 'css/app.scss';
+ReactDOM.render(<Router></Router>,document.querySelector("#root"));
